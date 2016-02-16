beb76d245eaa43f07d03655e5abba5bb82dc3cd7
rasteroid <rasteroid@users.noreply.github.com>
2016-02-16 17:10:18 -0500

    Updated stopWebServer.sh
    removed name source file

--- scripts/stopWebServer.sh
+++ scripts/stopWebServer.sh
@@ -8,8 +8,6 @@ node_type=$(ctx node type)
 
 ctx logger info "start ${node_type} BEGIN"
 
-. ./nameWS.sh
-
 sudo systemctl stop "$CLOUDIFY_WS_NAME.service"
 
 ctx logger info "start ${node_type} COMPLETED"
