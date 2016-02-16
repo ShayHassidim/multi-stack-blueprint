fd77a25eafcb6f4ae0cd13ffd5b87f79571b7975
rasteroid <rasteroid@users.noreply.github.com>
2016-02-16 16:33:35 -0500

    Updated stopLB.sh
    pull name from file

--- scripts/stopLB.sh
+++ scripts/stopLB.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 set -e
-  
+
 node_type=$(ctx node type)
 
-ctx logger info "stop ${node_type} " 
+ctx logger info "start ${node_type} BEGIN"
+
+. ./nameLB.sh
+
+systemctl stop "$CLOUDIFY_LB_NAME.service"
+
+ctx logger info "start ${node_type} COMPLETED"
 
