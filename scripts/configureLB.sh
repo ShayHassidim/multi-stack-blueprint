615a397cf7c2d90d5c14839d4ac704f0e380a71d
rasteroid <rasteroid@users.noreply.github.com>
2016-02-16 16:36:33 -0500

    Update configureLB.sh
    pull name from file
    check for previously installed
    stop & disable nginx
    set appropriate server name in httpd.conf

--- scripts/configureLB.sh
+++ scripts/configureLB.sh
@@ -1,8 +1,31 @@
 #!/bin/bash
-  
+
 set -e
 
 node_type=$(ctx node type)
 
-ctx logger info "configure ${node_type} " 
+ctx logger info "configure ${node_type} BEGIN"
 
+. ./nameLB.sh
+
+CLOUDIFY_LB_NAME_INSTALLED=`rpm -qa | grep $CLOUDIFY_LB_NAME`
+
+if [ -z "$CLOUDIFY_LB_NAME_INSTALLED" ] ; then
+		ctx logger info "configure ${node_type} ${CLOUDIFY_LB_NAME} already installed"
+		exit 2
+fi
+
+CLOUDIFY_NGINX_INSTALLED=`rpm -qa | grep nginx`
+
+if [ -z "$CLOUDIFY_NGINX_INSTALLED" ] ; then
+	systemctl stop nginx
+	systemctl disable nginx
+fi
+
+export HTTPD_CONF="/etc/httpd/conf/httpd.conf"
+
+yum -q -y install "$CLOUDIFY_LB_NAME"
+
+sed -i 's/ServerName\ www\.example\.com\:80/ServerName\ www\.example\.com\:80\nServerName\ '"$LB_IP_ADDR"':80/' $HTTPD_CONF
+
+ctx logger info "configure ${node_type} COMPLETED"
