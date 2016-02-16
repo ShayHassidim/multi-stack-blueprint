630260f27e65d02e4ad70fec052ac822aa5b93a7
rasteroid <rasteroid@users.noreply.github.com>
2016-02-16 16:55:41 -0500

    updateed startLB.sh
    exported service name locally
    added sudo to systemctl call

--- scripts/startLB.sh
+++ scripts/startLB.sh
@@ -2,12 +2,14 @@
 
 set -e
 
+export CLOUDIFY_LB_NAME=httpd
+
 node_type=$(ctx node type)
 
 ctx logger info "start ${node_type} BEGIN"
 
 . ./nameLB.sh
 
-systemctl start "$CLOUDIFY_LB_NAME.service"
+sudo systemctl start "$CLOUDIFY_LB_NAME.service"
 
 ctx logger info "start ${node_type} COMPLETED"
