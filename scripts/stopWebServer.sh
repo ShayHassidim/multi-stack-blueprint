d86297c7034647b4fa6f083863ade90bda0d8a4a
rasteroid <rasteroid@users.noreply.github.com>
2016-02-16 16:27:10 -0500

    Updated stopWebServer.sh
    pull name from file

--- scripts/stopWebServer.sh
+++ scripts/stopWebServer.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 set -e
-  
+
 node_type=$(ctx node type)
 
-ctx logger info "stop ${node_type} " 
+ctx logger info "start ${node_type} BEGIN"
 
+. ./nameWS.sh
+
+systemctl stop "$CLOUDIFY_WS_NAME.service"
+
+ctx logger info "start ${node_type} COMPLETED"
