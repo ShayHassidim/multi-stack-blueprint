b53bc5204cd5665f9aac6b9eecc9c0ab119f70b3
Shay Hassidim <shay@gigaspaces.com>
2016-02-22 16:41:53 -0500

    Update startAPPServer.sh
    added jboss start calls

--- scripts/startAPPServer.sh
+++ scripts/startAPPServer.sh
@@ -4,5 +4,12 @@ set -e
 
 node_type=$(ctx node type)
 
+cd jboss-as-7.1.1.Final/bin
+ctx logger info "about to start jboss..." 
+
+sudo ./standalone.sh -Djboss.bind.address=0.0.0.0 -Djboss.bind.address.management=0.0.0.0&
+
+ctx logger info "jboss started OK" 
+
 ctx logger info "start ${node_type} " 
 
