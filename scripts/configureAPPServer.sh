d760777a04f1d0e4b2cc96661e56ff6311422adc
Shay Hassidim <shay@gigaspaces.com>
2016-02-21 22:01:13 -0500

    Update configureAPPServer.sh
    added jboss install

--- scripts/configureAPPServer.sh
+++ scripts/configureAPPServer.sh
@@ -4,5 +4,18 @@ set -e
 
 node_type=$(ctx node type)
 
-ctx logger info "configure ${node_type} " 
+FILE=jboss-as-7.1.1.Final.zip
+if [ ! -f "$FILE" ]
+then
+    ctx logger info  "File $FILE does not exists - downloading..."
+	curl -O http://download.jboss.org/jbossas/7.1/jboss-as-7.1.1.Final/jboss-as-7.1.1.Final.zip
+    ctx logger info  "done downloading..."
+	sudo yum -y install unzip
+	sudo unzip jboss-as-7.1.1.Final.zip	
+    ctx logger info  "done installing jboss"
+else
+   ctx logger info  "jboss already installed"
+fi
+
+ctx logger info "configure ${node_type} done" 
 
