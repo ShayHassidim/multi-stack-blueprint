bf7901fdfa1283f33dc50f95280e6abe14b9f50a
rasteroid <rasteroid@users.noreply.github.com>
2016-02-29 10:40:58 -0500

    Update utilities.sh
    fixed spacing/indent issues

--- scripts/utilities.sh
+++ scripts/utilities.sh
@@ -6,8 +6,7 @@
 function package_is_installed() {
 	local INSTALLED=0
 	for PACKAGE in `yum list installed | grep $1`; do
-        if [[ $PACKAGE == "$1"* ]]
-        then
+	        if [[ $PACKAGE == "$1"* ]]; then
 			P=${PACKAGE%.*}
 			if [ "$P" == "$1" ]; then
 				INSTALLED=1
